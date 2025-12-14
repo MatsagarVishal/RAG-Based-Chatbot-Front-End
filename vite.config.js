a99de28bb32b003557d4670f83fd253ca52c46f5
Vishal Matsagar <vishalmatsagar0007@gmail.com>
2025-12-14 21:51:45 +0530

    added proxy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/proxy': {
+        target: 'http://13.60.207.30:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/proxy/, '')
+      }
+    }
+  }
 })
